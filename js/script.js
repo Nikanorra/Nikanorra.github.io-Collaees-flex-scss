529d558ab33be904b22640610160db371599ab03
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-10-26 16:41:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+	// $(document).ready(function(){
+	  // $('.slider__wrapper').slick({
+		//   dots: true,
+		//   infinite: true,
+		//   speed: 1000,
+		// 	autoplaySpeed: 5000,
+		//   fade: true,
+		//   cssEase: 'linear',
+		// 	autoplay: true,
+		// 	mobileFirst: true
+		//
+		// });
+ $(document).ready(function(){
+		$('.menu__burger').click(function(event) {
+			$('.menu__burger, .menu').toggleClass('active');
+		});
+
+	});
+
+//
+// 	$(document).ready(function() {
+// 			$('.burger').click(function(event) {
+// 				$('.burger, .menu').toggleClass('active');
+// 			});
+// });
